8a0677eb5e08d210edbf90d4b15da02fbaecca0c
VladislavFomenko <v.fomenko@pinglestudio.com>
2021-01-15 18:29:18 +0200

    [+] Take damage

--- Assets/Scripts/Player/CameraFolow.cs
+++ Assets/Scripts/Player/CameraFolow.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFolow : MonoBehaviour
+namespace Player
 {
-    [SerializeField]
-    private GameObject playerCar = null;
-
-    [SerializeField]
-    private float speed = 5f;
-    
-    private Vector3 newPosition = new Vector3();
-
-    void Update()
+    public class CameraFolow : MonoBehaviour
     {
-        if(transform.position.z != playerCar.transform.position.z)
+        [SerializeField]
+        private GameObject playerCar = null;
+
+        [SerializeField]
+        private float speed = 5f;
+
+        private Vector3 newPosition = new Vector3();
+
+        void Update()
         {
-            newPosition.Set(transform.position.x, transform.position.y, playerCar.transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+            if (transform.position.z != playerCar.transform.position.z)
+            {
+                newPosition.Set(transform.position.x, transform.position.y, playerCar.transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Player/HealthManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class HealthManager : MonoBehaviour
+    {
+        public static HealthManager instance;
+
+        public static Action<int> E_UpdateHealth;
+        public static Action E_Die;
+
+        [SerializeField]
+        private bool isTakeDamage = true;
+
+        public static int health { get; private set; } = 100;
+        public static int maxHealth { get; private set; } = 100;
+
+        public static bool isAlive { get; private set; } = true;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void OnDestroy()
+        {
+            if (instance.Equals(this))
+                instance = null;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (!isTakeDamage) return;
+
+            health -= damage;
+            E_UpdateHealth?.Invoke(health);
+
+            if (health <= 0)
+            {
+                E_Die?.Invoke();
+                isAlive = false;
+            }
+        }
+
+        public void RestoreHealth(int restore)
+        {
+            health = health + restore > maxHealth ? maxHealth : health + restore;
+            E_UpdateHealth?.Invoke(health);
+        }
+    }
+}
