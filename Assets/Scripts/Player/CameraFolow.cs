fbf139d2dd391ddb1cd117174130f55ed39de8ca
morentu <vlad981125@gmail.com>
2021-01-15 20:49:59 +0200

    [+] Add clamp camera position

--- Assets/Scripts/Player/CameraFolow.cs
+++ Assets/Scripts/Player/CameraFolow.cs
@@ -11,14 +11,19 @@ namespace Player
 
         [SerializeField]
         private float speed = 5f;
+        [Header("Clamp camera position")]
+        [SerializeField]
+        private float maxLeft = 5;
+        [SerializeField]
+        private float maxRight = -5;
 
         private Vector3 newPosition = new Vector3();
 
         void Update()
         {
-            if (transform.position.z != playerCar.transform.position.z)
+            if (transform.position.z != playerCar.transform.position.z && transform.position.z > maxRight && transform.position.z < maxLeft)
             {
-                newPosition.Set(transform.position.x, transform.position.y, playerCar.transform.position.z);
+                newPosition.Set(transform.position.x, transform.position.y, Mathf.Clamp(playerCar.transform.position.z, maxRight, maxLeft));
                 transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
             }
         }
