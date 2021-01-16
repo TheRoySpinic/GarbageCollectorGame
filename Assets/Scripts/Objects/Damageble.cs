fcd5ae8f1f31a4099c7b0c2ca81b1a9fd0e9caba
VladislavFomenko <v.fomenko@pinglestudio.com>
2021-01-16 15:42:33 +0200

    [~] Trafic fence physics

--- Assets/Scripts/Objects/Damageble.cs
+++ Assets/Scripts/Objects/Damageble.cs
@@ -12,10 +12,37 @@ namespace Map
 
         private bool isActive = true;
 
+        [SerializeField]
+        private bool frezeWhileNoDamage = false;
+
+        private Rigidbody rb = null;
+
+        private void Awake()
+        {
+            if(frezeWhileNoDamage)
+            {
+                rb = GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+                foreach(Rigidbody r in transform.GetComponentsInChildren<Rigidbody>())
+                {
+                    r.constraints = RigidbodyConstraints.FreezeAll;
+                }
+            }
+        }
+
         private void OnCollisionEnter(Collision collision)
         {
             if (isActive && collision.gameObject.tag.Equals("Player"))
             {
+                if (frezeWhileNoDamage)
+                {
+                    rb.constraints = RigidbodyConstraints.None;
+                    foreach (Rigidbody r in transform.GetComponentsInChildren<Rigidbody>())
+                    {
+                        r.constraints = RigidbodyConstraints.None;
+                    }
+                }
+
                 HealthManager.instance.TakeDamage(damage);
                 isActive = false;
             }
