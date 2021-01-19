d1648cd31867b614293e9972131b0a663f04db6c
VladislavFomenko <v.fomenko@pinglestudio.com>
2021-01-19 11:19:52 +0200

    [+] Add garbageSpot script

---
+++ Assets/Scripts/Design/GarbageSpot.cs
@@ -0,0 +1,44 @@
+﻿using Balance;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Target;
+using UnityEngine;
+
+namespace Design
+{
+    public class GarbageSpot : MonoBehaviour
+    {
+        [SerializeField]
+        private Spot[] spots = null;
+
+        private void Awake()
+        {
+            foreach(Spot spot in spots)
+            {
+                if (UnityEngine.Random.Range(0, 1f) < Array.Find(GameBalance.GetMapBalance().garbageSpawnsConfig, 
+                                                      (a) => { return a.garbageType == spot.garbageType; }).spawnChange)
+                {
+                    foreach (GameObject o in spot.garbage)
+                    {
+                        o.SetActive(true);
+                    }
+                }
+                else
+                {
+                    foreach (GameObject o in spot.garbage)
+                    {
+                        o.SetActive(false);
+                    }
+                }
+            }
+        }
+
+        [Serializable]
+        private class Spot
+        {
+            public GarbageType garbageType = GarbageType.NONE;
+            public GameObject[] garbage = null;
+        }
+    }
+}
