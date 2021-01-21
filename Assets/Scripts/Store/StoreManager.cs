012e27755cadc8cb7f03fc8bd6b032dff35ece5f
VladislavFomenko <v.fomenko@pinglestudio.com>
2021-01-21 16:15:48 +0200

    @ progress

--- Assets/Scripts/Store/StoreManager.cs
+++ Assets/Scripts/Store/StoreManager.cs
@@ -8,15 +8,22 @@ namespace Store
 {
     public class StoreManager : MonoBehaviour
     {
+        private const string PLAYERPREFS_PLAYER_COLORSETS_FIELD = "player_colorsets";
+
         public static StoreManager instance = null;
 
         public static Action E_GoldUpdate;
+        public static Action E_PlayerColorsetUpdate;
+        public static Action E_PlayerColorsetsLoaded;
 
         private static int _gold = 0;
         public static int gold { get { return _gold; } private set { _gold = value; PlayerPrefs.SetInt("gold", value); E_GoldUpdate?.Invoke(); } }
 
-        //[SerializeField]
-        //private Colorset[] colorsets = null;
+        [SerializeField]
+        private Colorset[] colorsets = null;
+
+        [SerializeField]
+        private PlayerColorsets playerColorsets = new PlayerColorsets();
 
         private void Awake()
         {
@@ -25,6 +32,9 @@ namespace Store
 
             _gold = PlayerPrefs.GetInt("gold");
             E_GoldUpdate?.Invoke();
+            
+            if(LoadPlayerColorsets())
+                E_PlayerColorsetsLoaded?.Invoke();
         }
 
         private void OnDestroy()
@@ -32,13 +42,46 @@ namespace Store
             if (instance.Equals(this))
                 instance = null;
         }
+        
+        public Texture GetColorsetMainTexture(EColorsetType colorsetType)
+        {
+            return Array.Find(colorsets, (c) => { return c.colorset == colorsetType; }).maintTexture;
+        }
+        
+        public int GetColorsetCost(EColorsetType colorsetType)
+        {
+            return Array.Find(colorsets, (c) => { return c.colorset == colorsetType; }).cost;
+        }
 
+
+        private void SavePlayerColorsets()
+        {
+            PlayerPrefs.SetString(PLAYERPREFS_PLAYER_COLORSETS_FIELD, JsonUtility.ToJson(playerColorsets));
+        }
+
+        private bool LoadPlayerColorsets()
+        {
+            if (PlayerPrefs.GetString(PLAYERPREFS_PLAYER_COLORSETS_FIELD).Length > 0)
+            {
+                playerColorsets = JsonUtility.FromJson<PlayerColorsets>(PlayerPrefs.GetString(PLAYERPREFS_PLAYER_COLORSETS_FIELD));
+                return true;
+            }
+            return false;
+        }
+
+        [Serializable]
+        private class PlayerColorsets
+        {
+            public EColorsetType activeColorset = EColorsetType.DEFAULT;
+            public List<EColorsetType> colorsets = new List<EColorsetType>();
+        }
 
         [Serializable]
         private class Colorset
         {
             public EColorsetType colorset = EColorsetType.DEFAULT;
-            public Sprite albedo = null;
+            public Texture maintTexture = null;
+            public int cost = 1;
 
         }
     }
