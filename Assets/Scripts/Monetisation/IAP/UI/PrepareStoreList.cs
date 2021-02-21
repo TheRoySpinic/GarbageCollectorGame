ac2e8002f1dd0b668d0e4f843a2b3b80a961eab0
morentu <vlad981125@gmail.com>
2021-02-21 23:30:32 +0200

    [+] Import unity IAP

---
+++ Assets/Scripts/Monetisation/IAP/UI/PrepareStoreList.cs
@@ -0,0 +1,39 @@
+﻿using HUD;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monetisation.IAP.UI
+{
+    public class PrepareStoreList : MonoBehaviour
+    {
+        public static PrepareStoreList instance = null;
+
+        [SerializeField]
+        private GameObject content = null;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+
+            ScreensManager.E_ShowBuyStore -= PrepareStore;
+            ScreensManager.E_ShowBuyStore += PrepareStore;
+        }
+
+        private void OnDestroy()
+        {
+            ScreensManager.E_ShowBuyStore -= PrepareStore;
+        }
+
+        public void PrepareStore()
+        {
+            ClearList();
+        }
+
+        private void ClearList()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Monetisation/IAP/UI/StoreGroup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monetisation.IAP.UI
+{
+    public class StoreGroup : MonoBehaviour
+    {
+        public void FillGroup(ECurrencyType currencyType)
+        {
+
+        }
+    }
+}
