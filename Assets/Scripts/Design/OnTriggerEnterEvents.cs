01d710e2637405ba575dec70cbedb16ee793578b
morentu <vlad981125@gmail.com>
2021-03-21 16:42:07 +0200

    [+] Add design components

---
+++ Assets/Scripts/Design/OnTriggerEnterEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Design
+{
+    public class OnTriggerEnterEvents : MonoBehaviour
+    {
+        [Header("Check tag other object (if clear use all tags)")]
+        [SerializeField]
+        private string onlyTag = "";
+
+        [SerializeField]
+        private UnityEvent unityEvent = new UnityEvent();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(onlyTag.Equals("") || other.gameObject.tag.Equals(onlyTag))
+            {
+                unityEvent?.Invoke();
+            }
+        }
+    }
+}
