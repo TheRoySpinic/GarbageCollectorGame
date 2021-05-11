8fa1cb4334bcdf591e1ff85c9cb7d896a7fa0f21
_Spinic_ <spinogryzzz@gmail.com>
2021-05-11 17:56:48 +0300

    @progress

---
+++ Assets/Scripts/Player/TouchControll/TouchController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Player.Control
+{
+    public class TouchController : MonoBehaviour
+    {
+        public static Action<Vector2Int> OnSwipe;
+
+        [SerializeField]
+        private float minSwipeDistance = 0;
+
+        [SerializeField]
+        private float xDistance = 0;
+        private float yDistance = 0;
+
+        private bool swipeDetection = false;
+
+        public void BeginTouch()
+        {
+            swipeDetection = true;
+        }
+
+        public void MoveTouch()
+        {
+            if (!swipeDetection)
+                return;
+
+            xDistance += Input.GetTouch(0).deltaPosition.x;
+            yDistance += Input.GetTouch(0).deltaPosition.y;
+
+            if(Mathf.Abs(xDistance) > minSwipeDistance || Mathf.Abs(yDistance) > minSwipeDistance)
+            {
+                Vector2Int vector = new Vector2Int(
+                    xDistance > 0 ? 1 : xDistance < 0 ? -1 : 0,
+                    yDistance > 0 ? 1 : yDistance < 0 ? -1 : 0
+                    );
+
+                OnSwipe?.Invoke(vector);
+
+                swipeDetection = false;
+
+                xDistance = 0;
+                yDistance = 0;
+            }
+        }
+
+        public void EndTouch()
+        {
+            swipeDetection = false;
+
+            xDistance = 0;
+            yDistance = 0;
+        }
+    }
+}
